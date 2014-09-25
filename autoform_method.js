535700fc3ab5960e287fbe7b408895eaf2d2d867
Jakob Dam Jensen <jakob.jensen@creuna.dk>
2014-09-25 14:07:49 +0200

    initial

---
+++ autoform_method.js
@@ -0,0 +1,31 @@
+TestSchema = new SimpleSchema({
+    title: {
+        type: String
+    }
+});
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault("counter", 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get("counter");
+    }
+  });
+
+  Template.hello.events({
+    'click button': function (e, template) {
+      // increment the counter when button is clicked
+      template.find("form").submit();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+    Meteor.methods({
+        insertdata: function(data){
+            console.log(data);
+        }
+    })
+}
