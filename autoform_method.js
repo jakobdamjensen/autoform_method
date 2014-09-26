dc9e947f3a5ee109feb7c67fca28867ece40de36
Jakob Dam Jensen <jakob@nopointlessapps.com>
2014-09-26 14:11:53 +0200

    Removing unused stuff

--- autoform_method.js
+++ autoform_method.js
@@ -5,15 +5,6 @@ TestSchema = new SimpleSchema({
 });
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
   Template.hello.events({
     'click button#submitButtonOutside': function (e, template) {
       // increment the counter when button is clicked
